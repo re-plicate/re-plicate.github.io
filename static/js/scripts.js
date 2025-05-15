024c76967b1719963e0e636991260b97baa2d2d0
Jinhui Ouyang <ouyangjinhui23@gmail.com>
2025-05-15 17:51:15 +0800

    V5

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,8 +1,6 @@
-
-
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'awards']
+const section_names = ['home', 'publications', 'awards', 'experience']
 
 
 window.addEventListener('DOMContentLoaded', event => {
